16639273ee8407a86d28b8767309c88dee2fa6f4
morteza <mori2002sa@gmail.com>
2024-04-18 00:10:13 +0330

    Remove console.log

--- src/pages/CabinsPage.tsx
+++ src/pages/CabinsPage.tsx
@@ -1,11 +1,6 @@
 import { CabinsTable } from "@/components/cabins";
-import { getCabins } from "@/services/apiCabins";
-import { useEffect } from "react";
 
 const CabinsPage = () => {
-  useEffect(() => {
-    getCabins().then((data) => console.log(data));
-  });
   return <CabinsTable />;
 };
 
